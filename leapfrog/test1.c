dc357b23a95d1c86a7eaece7b9333fd83acbcd24
Umesh Shrestha <70258765+shres58tha@users.noreply.github.com>
2023-10-15 07:56:56 +0545

    Update test1.c
    bug  fix

--- leapfrog/test1.c
+++ leapfrog/test1.c
@@ -24,15 +24,22 @@ int logic(int x, int y)  // function to find if x is power of y if yes return 1 
 // we need to just modify function to fulfill the task this was the only one i had time to do
 // just went through to see all the question thou
 {
-   int temp=1, flag =0;
-    
-   for (int i=0;temp<=x;i++){
-        temp=temp*y;
-        if (temp==x){
-            flag =1;
-            break;
-        }
+   if (y==1){
+        if (x==1)
+            return 1;
+        else
+            return 0;
+   }
+   else { 
+        int temp=1, flag =0;
+        for (int i=0;temp<=x;i++){
+            temp=temp*y;
+            if (temp==x){
+                flag =1;
+                break;
+            }
         
-    }
+        }
     return flag;
+   }
 }
